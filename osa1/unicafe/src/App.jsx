2f6c28737d4e6ac93fbc362027de6ca6c44d7ebb
ellaverak <ella.korkeaaho@gmail.com>
2024-11-15 22:51:46 +0200

    modify App.jsx

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -14,48 +14,60 @@ const Button = (props) => (
   </button>
 )
 
-const Display = (props) => (
-  <div>{props.text} {props.value}</div>
-)
-
-const Total = (props) => (
-  <Display value={props.good + props.neutral + props.bad} text="all" />
-)
-
-const Average = (props) => {
+const Statistics = (props) => {
   const total = props.good + props.neutral + props.bad
   const average = (1*props.good + -1*props.bad)/total
+  const positive = props.good/(props.good + props.neutral + props.bad)
 
   return (
-  <Display value={average} text="average" />
+    <table>
+      <tbody>
+        <StatisticLine value={props.good} text="good" />
+        <StatisticLine value={props.neutral} text="neutral" />
+        <StatisticLine value={props.bad} text="bad" />
+        <StatisticLine value={total} text="all" />
+        <StatisticLine value={average} text="average" />
+        <StatisticLine value={positive} text="positive" />
+      </tbody>
+    </table>
   )
 }
 
-const Positive = (props) => (
-  <Display value={props.good/(props.good + props.neutral + props.bad)} text="positive" />
+const StatisticLine = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+  </tr>
 )
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
+  if (good !== 0 || neutral !== 0 || bad !== 0) {
+    return (
+      <div>
+        <Header header="give feedback" />
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+        <Header header="statistics" />
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+    )
+  } else {
+    return (
     <div>
       <Header header="give feedback" />
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Header header="statistics" />
-      <Display value={good} text="good" />
-      <Display value={neutral} text="neutral" />
-      <Display value={bad} text="bad" />
-      <Total good={good} neutral={neutral} bad={bad} />
-      <Average good={good} neutral={neutral} bad={bad} />
-      <Positive good={good} neutral={neutral} bad={bad} />
+      <p>No feedback given</p>
     </div>
-  )
+    )
+  }
 }
 
 export default App
